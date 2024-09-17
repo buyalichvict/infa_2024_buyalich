85294ad6a46517e2ab0d6d03714fcdd694df795e
buyalichvict <buyalich.s22@gmail.com>
2024-09-17 17:45:06 +0300

    exercise1

---
+++ cho-to1.py
@@ -0,0 +1,15 @@
+import turtle
+
+def crimson():
+    turtle.forward(20)
+    turtle.right(135)
+    turtle.forward(30)
+    turtle.left(45)
+    turtle.forward(20)
+ 
+turtle.penup()
+turtle.goto(20, 20)
+turtle.pendown()
+crimson()
+
+turtle.mainloop()
